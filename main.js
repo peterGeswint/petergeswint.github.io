c2a82014ca38d23b88bf939c3a7ace46bf71d7eb
Peter Clifton Geswint <115699568+peterGeswint@users.noreply.github.com>
2025-02-07 23:44:39 +0200

    type effect and added copy

---
+++ main.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".typedText", {
+    strings: ["Software Developer", "UX/UI Designer", "Data Analyst"], // Fixed "strings" and removed extra space
+    loop: true,
+    typeSpeed: 130,
+    backSpeed: 110,
+    backDelay: 2000
+});
